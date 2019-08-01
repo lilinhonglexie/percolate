b9fe2399841d610e76d727dbcd260d24c3fd78b8
Linhong Li <linhongl@Linhongs-MBP.wv.cc.cmu.edu>
2019-08-01 13:22:58 -0400

    Update doc and working space

--- R/utils.R
+++ R/utils.R
@@ -1,6 +1,17 @@
 
 library(assertthat)
 
+#' Generate a legal board per inputted instructions.
+#'
+#' @param n A positive integer denoting the dimension of the square board.
+#' @param p A float between 0 to 1 that denotes the fraction
+#' of the n^2 squares are blocked
+#'
+#' @return The n-by-n board with floor(p*n^2) blocked squares
+#'
+#' @examples generate_board_mat(n = 8, p = 0.75)
+#'
+#' @export
 generate_board_mat <- function(n=5, p=0.25){
   assert_that(n > 0)
   assert_that(0 <= p && p <= 1)
@@ -11,6 +22,3 @@ generate_board_mat <- function(n=5, p=0.25){
   result_mat <- matrix(result_vec, nrow = n, ncol = n)
   return(result_mat)
 }
-
-generate_board_mat()
-generate_board_mat(n = 8, p = 0.75)
